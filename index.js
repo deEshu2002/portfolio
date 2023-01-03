88fde5f59f8b43b71b4152886564f0f0bcba7905
Deepanshu <deepanshumehra2001@gmail.com>
2023-01-03 14:06:12 +0530

    made the layout to follow grid

--- index.js
+++ index.js
@@ -8,8 +8,28 @@ plus.addEventListener("click", function() {
         if(link.style.animation) {
             link.style.animation = "";
         } else{
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index/10 +0.5}s`;
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index/10 +0.1}s`;
         }
     }) 
 
 })
+
+
+let selectButton = document.querySelector(".select-button");
+let currentGenre = document.querySelectorAll(".options li");
+
+let genre = document.getElementsByTagName("p")[0].innerHTML;
+console.log(genre);
+
+
+selectButton.addEventListener("click", () => {
+    currentGenre.forEach((link, index) => {
+        if(link.style.animation) {
+            link.style.animation = "";
+        } else{
+            link.style.animation = `navLinkFade 0.5s ease forwards  0.2s`;
+        }
+    })
+})
+
+
